93ec3a46d050e870029be0e28f331775737833c8
Mulinatti <gabrielmulinari2002@gmail.com>
2023-05-23 22:03:31 -0300

    valida se todos os campos estao preenchidos

---
+++ app/sign-page/sign-js/validacao.js
@@ -0,0 +1,35 @@
+const btnCadastro = document.getElementById("cadastrar")
+const inputs = document.querySelectorAll(".inputStyle");
+const aviso = document.getElementById("aviso");
+
+let numeroDeCamposFaltando;
+
+const validarUsuario = (campo) => {
+
+    if(campo.value.length == 0) {
+        numeroDeCamposFaltando += 1;
+        campo.classList.add("border-red-500");
+        aviso.classList.add("block")
+        aviso.classList.remove("invisible")
+
+    }
+    else if(campo.value.length != 0) {
+        campo.classList.remove("border-red-500")
+    }
+    
+    if(numeroDeCamposFaltando == 0) {
+        aviso.classList.add("invisible");
+    }
+    console.log(numeroDeCamposFaltando);
+}
+
+btnCadastro.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    numeroDeCamposFaltando = 0;
+
+    inputs.forEach(input => {
+        validarUsuario(input);
+    })
+});
+
