09a5e84955949254dddd29c05d2e309b854a9bf7
JoaoMathus <joaomatheusGDO@gmail.com>
2023-06-07 08:15:32 -0300

    alomomola.

--- app/profile-page/profile-index.php
+++ app/profile-page/profile-index.php
@@ -100,8 +100,8 @@
             <div class="flex flex-row items-start">
                 <figure class="flex flex-row items-center justify-end">
                     <!--<img class="rounded-full w-[80px] h-[80px] shadow-md" src="../../src/imgs/bannerservice.png" alt="anon">-->
-                    <img class="rounded-full w-[80px] h-[80px] shadow-md" src="https://conteudo.imguol.com.br/c/noticias/8c/2022/11/22/02mai2022---o-bilionario-elon-musk-no-met-gala-em-nova-york-1669125518979_v2_1x1.jpg<?php //echo '../../'.$_SESSION["foto"]; ?>" alt="Foto de perfil">
-                    <figcaption class="text-2xl text-center w-full ml-4">Elon Musk<?php //echo $_SESSION["usuario"] ?></figcaption>
+                    <img class="rounded-full w-[80px] h-[80px] shadow-md" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a09d249f-32ca-4b05-8682-915b4fd70684/damijb6-e37baa19-a1db-4c9c-827b-9042ed1ea085.png/v1/fill/w_742,h_1077/mega_alomomola_by_crazyphylis_damijb6-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQ4NyIsInBhdGgiOiJcL2ZcL2EwOWQyNDlmLTMyY2EtNGIwNS04NjgyLTkxNWI0ZmQ3MDY4NFwvZGFtaWpiNi1lMzdiYWExOS1hMWRiLTRjOWMtODI3Yi05MDQyZWQxZWEwODUucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.1XfKDmGgyv4O95s6UjDlpijah6FxuNHlEHnjVN27_oo<?php //echo '../../'.$_SESSION["foto"]; ?>" alt="Foto de perfil">
+                    <figcaption class="text-2xl text-center w-full ml-4">Mega Alomomola<?php //echo $_SESSION["usuario"] ?></figcaption>
                 </figure>
             </div>
             <div class="flex flex-col justify-center items-center mt-5 md:flex-row sm:mt-0">
